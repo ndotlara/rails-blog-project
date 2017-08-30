087ac4a223d70b272e0753027516c9ea1ed1f776
ndotlara <natalialara.cd@gmail.com>
2017-08-30 02:35:18 -0400

    updated get-comments script

---
+++ scripts/comments/get-comments.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+API="${API_ORIGIN:-http://localhost:4741}"
+# API="${API_ORIGIN:-https://rails-blog-project.herokuapp.com/}"
+URL_PATH="/article/${ID}/comments/${ID}"
+curl "${API}${URL_PATH}" \
+  --include \
+  --request PATCH \
+  --header "Content-Type: application/json" \
+
+echo
